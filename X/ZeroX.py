bbf127cc50db5fc208e226d0c8d8edf3656f2e78
Anatoliy Levchuk <feuerlag999@gmail.com>
2020-05-02 13:49:27 +0300

    Прописано условие победы, требуется довавить проверку ввода, закрытие цикла, выбор игрока

---
+++ X/ZeroX.py
@@ -0,0 +1,58 @@
+'''
+Входное задание PY110 Срок заканчивается 29 мая 2020 г., 23: 59
+Инструкции До конца курса PY110, а именно до зачетного занятия, необходимо выполнить входное задание.
+Оно представляет собой консольное приложение, реализующее игру в крестики - нолики.
+В игре должна быть возможность играть двум игрокам.После запроса хода, должна производиться проверка его корректности,
+в случае успешности производится проверка, является ли данное состояние победным.
+Результатом выполнения должен быть исходные код приложения в любом формате.Файл с раширением.py либо репозиторий на git
+'''
+import numpy as np
+
+def condition_func(matrix, n):
+
+    for j in range(1, n + 1):
+        if all(matrix[0][i][i] == 'X' for i in range(1, n + 1)) \
+                or all(matrix[0][i][j] == 'X' for i in range(1, n + 1)) \
+                or all(matrix[0][j][i] == 'X' for i in range(1, n + 1)) \
+                or all(matrix[0][i][n + 1 - i] == 'X' for i in range(1, n + 1)):
+            if_exit = "-----------Победа игрока X-er-----------"
+
+        elif all(matrix[0][i][i] == 'O' for i in range(1, n + 1)) \
+                or all(matrix[0][i][j] == 'O' for i in range(1, n + 1)) \
+                or all(matrix[0][j][i] == 'O' for i in range(1, n + 1)) \
+                or all(matrix[0][i][n + 1 - i] == 'O' for i in range(1, n + 1)):
+            if_exit = "-----------Победа игрока O-er ----------"
+        else:
+            pass
+
+def ZeroX_Field(condition_func, n=3):
+    matrix = np.zeros((1, n + 1, n + 1), dtype=str)
+    for i in range(0, n + 1, 1):
+        matrix[0][i][0] = i
+    for j in range(0, n + 1, 1):
+        matrix[0][0][j] = j
+    for i in range(1, n + 1, 1):
+        for j in range(1, n + 1):
+            matrix[0][i][j] = '_'
+    print(matrix, end='\n')
+
+    counter = 0
+
+    while True:
+        if counter % 2 == 0:
+            simbol = 'X'
+        elif counter % 2 != 0:
+            simbol = 'O'
+        print(f"Ход игрока {simbol}-er")
+        s = input(f"Введите номер строки от 1 до {n}: ")
+        d = input(f"Введите номер столбцаот 1 до {n}: ")
+        matrix[0, int(s), int(d)] = simbol
+        print(matrix, end='\n')
+        counter += 1
+        condition_func(matrix, n)
+        if counter >= n ** 2:
+            print("------------Ничья-----------")
+        if condition_func:
+            break
+
+ZeroX_Field(condition_func)
