1518d37e17b7b10496028000db11c6aa0ddd8eaa
Anatoliy Levchuk <feuerlag999@gmail.com>
2020-05-12 22:21:36 +0300

    PEP 8 ПРАВКИ

--- X/ZeroX.py
+++ X/ZeroX.py
@@ -1,28 +1,30 @@
-'''
+"""
 Входное задание PY110 Срок заканчивается 29 мая 2020 г., 23: 59
 Инструкции До конца курса PY110, а именно до зачетного занятия, необходимо выполнить входное задание.
 Оно представляет собой консольное приложение, реализующее игру в крестики - нолики.
 В игре должна быть возможность играть двум игрокам.После запроса хода, должна производиться проверка его корректности,
 в случае успешности производится проверка, является ли данное состояние победным.
 Результатом выполнения должен быть исходные код приложения в любом формате.Файл с раширением.py либо репозиторий на git
-'''
+"""
+
 import numpy as np
 
-n = 3       # Величина игрового поля (n x n)
-counter = 0     # Счетчик ходов
+n = 3  # Величина игрового поля (n x n)
+counter = 0  # Счетчик ходов
 simbol = ""
 status = "game"
 
+
 # Создание игрового поля
-def ZeroX_Field(n):
+def zero_x_field(n):
     matrix = np.full((n, n), '_', dtype=str)
     matrix[:, 0] = range(0, n)
     matrix[0, :] = range(0, n)
     print(matrix, end='\n')
     return matrix
 
+
 # Условие победы
-# TODO: убрать for, реализовать проверку через np
 def condition_func(matrix, simbol):
     if ((np.diagonal(matrix) == simbol).all()).any() \
             or ((np.diagonal(np.rot90(matrix)) == simbol).all()).any() \
@@ -31,40 +33,41 @@ def condition_func(matrix, simbol):
         status = 'over'
         return status
 
+
 # Обработка ошибок при введении координат поля
-def digit_exeption(s, d, matrix):
+def digit_exeption(matrix):
     while True:
         try:
-            while matrix[int(s)][int(d)] == 'X' \
-                    or matrix[int(s)][int(d)] == 'O':
+            s = input(f"Введите номер строки от 0 до {n - 1}: ")
+            d = input(f"Введите номер столбца от 0 до {n - 1}: ")
+            if matrix[int(s)][int(d)] != 'X' \
+                    and matrix[int(s)][int(d)] != 'O':
+                matrix[int(s), int(d)] = simbol
+                break
+            else:
                 print("*****Это поле уже занято, введите координаты другого поля: ")
-                s = input(f"Введите номер строки от 0 до {n-1}: ")
-                d = input(f"Введите номер столбцаот 0 до {n-1}: ")
-        except:
+        except ValueError:
             print("*****Вы ввели некорректные координаты*****")
-            s = input(f"Введите номер строки от 0 до {n-1}: ")
-            d = input(f"Введите номер столбцаот 0 до {n-1}: ")
-        else:
-            matrix[int(s), int(d)] = simbol
-            break
-matrix = ZeroX_Field(n)
+        except IndexError:
+            print("*****Вы ввели некорректные координаты*****")
+
+
+matrix = zero_x_field(n)
 
 # Тело игры
-while status == "game":
+while status:
     if counter % 2 == 0:
         simbol = 'X'
     elif counter % 2 != 0:
         simbol = 'O'
 
-    s = input(f"Ход игрока {simbol}-er \nВведите номер строки от 0 до {n-1}: ")
-    d = input(f"Введите номер столбцаот 0 до {n-1}: ")
-
-    digit_exeption(s, d, matrix)    # Проверка корректности хода
+    print(f"Ход игрока {simbol}-er")
+    digit_exeption(matrix)  # Проверка корректности хода
 
     print(matrix, end='\n')
     counter += 1
     if counter >= n:
-        condition_func(matrix, simbol)       # Проверка победного состояния
+        condition_func(matrix, simbol)  # Проверка победного состояния
 
     if condition_func(matrix, simbol) == 'over':
         print(f"-----------Победа игрока {simbol}-er-----------")
@@ -72,4 +75,3 @@ while status == "game":
     elif counter >= n ** 2:
         print("------------Ничья-----------")
         break
-
